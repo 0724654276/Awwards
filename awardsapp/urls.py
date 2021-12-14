f08d17a0d5b398ca78e2f79b4978112f3f075693
0724654276 <you@example.com>
2021-12-14 09:08:24 +0300

    add css in index.css

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import ListProfileView, ListProjectView
+
 urlpatterns=[
     path('',views.index, name = 'index'),
     path('register/', views.register, name='register'),
